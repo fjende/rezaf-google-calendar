b5dff705adf6f71f619253a90b24ea76517c20a7
Nema Problema <37379898+fjende@users.noreply.github.com>
2020-02-18 16:45:46 +0100

    styled login/logout

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,8 +1,15 @@
 import React, { useEffect } from 'react'
 import { GoogleLogout } from 'react-google-login';
 import { CLIENT_ID } from '../../../api';
+import { Button, Navbar } from 'react-bootstrap';
 
 function Home(props) {
+
+    useEffect(() => {
+        if (sessionStorage.getItem('loggedIn') == null) {
+            props.history.push('/');
+        }
+    });
 
     const logoutSuccess = () => {
         sessionStorage.removeItem('loggedIn');
@@ -11,16 +18,22 @@ function Home(props) {
 
     return (
         <div>
-            <GoogleLogout
-                clientId={CLIENT_ID}
-                buttonText="Logout"
-                onLogoutSuccess={logoutSuccess}
-                render={renderProps => (
-                    <button onClick={renderProps.onClick} disabled={renderProps.disabled}>logout</button>
-                )}
+            <Navbar bg="light">
+                <Navbar.Brand> Google Calendar Apllication</Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                    <GoogleLogout
+                        clientId={CLIENT_ID}
+                        buttonText="Logout"
+                        onLogoutSuccess={logoutSuccess}
+                        render={renderProps => (
+                            <Button color="primary" onClick={renderProps.onClick} disabled={renderProps.disabled}>Logout</Button>
+                        )}
 
-            >
-            </GoogleLogout>
+                    >
+                    </GoogleLogout>
+                </Navbar.Collapse>
+            </Navbar>
         </div>
     );
 }
--- src/Components/Pages/Login/Login.js
+++ src/Components/Pages/Login/Login.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import GoogleLogin from 'react-google-login'
 import { CLIENT_ID } from '../../../api';
+import { Jumbotron, Button } from 'react-bootstrap';
 
 export default function Login(props) {
 
@@ -22,16 +23,23 @@ export default function Login(props) {
 
     return (
         <div>
-            <GoogleLogin
-                clientId={CLIENT_ID}
-                buttonText="Login"
-                render={renderProps => (
-                    <button onClick={renderProps.onClick} disabled={renderProps.disabled}>login</button>
-                )}
-                onSuccess={responseSuccess}
-                onFailure={responseFailure}
-                cookiePolicy={'single_host_origin'}
-            />
+            <Jumbotron >
+                <h1 className="display-3">Hello, world!</h1>
+                <p className="lead">Welcome to my demo Google Calendar Apllication.</p>
+                <p>This app was made for a React Engineer assigment @ Ars Futura, in February 2020.</p>
+                <p className="lead">
+                    <GoogleLogin
+                        clientId={CLIENT_ID}
+                        buttonText="Login"
+                        render={renderProps => (
+                            <Button color="primary" onClick={renderProps.onClick} disabled={renderProps.disabled}>Login with your Google account</Button>
+                        )}
+                        onSuccess={responseSuccess}
+                        onFailure={responseFailure}
+                        cookiePolicy={'single_host_origin'}
+                    />
+                </p>
+            </Jumbotron>
         </div>
     )
 }
